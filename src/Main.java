ed96bfe897a8d205fba7e605fa3c97a7367fb733
Kshitij Pandey <pandeytij15@gmail.com>
2022-10-11 12:56:14 +0100

    Upadating changes to paint calculator.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,65 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int Room_weidth_metres = 3;
-        double Room_height_metres = 2.7d;
-        double Room_length_metres = 3.3d;
-        double Room_surface_area_metre_squared = (2*Room_length_metres*Room_height_metres + Room_length_metres*Room_weidth_metres + 2*Room_height_metres*Room_weidth_metres);
-        int Amount_of_paint_required_per_metre_squared_in_ml = 100;
-        double Amount_of_paint_required_to_paint_room_in_ml = Amount_of_paint_required_per_metre_squared_in_ml * Room_surface_area_metre_squared;
+    Scanner user_input = new Scanner(System.in);
+    // declaring room dimensions in metres (line 8-11 using user input)
+        double RoomWeidth;
+        System.out.println("Enter Weidth of room: ");
+        RoomWeidth = user_input.nextDouble() ;
+
+        double RoomHeight;
+        System.out.println("Enter Height of room: ");
+        RoomHeight = user_input.nextDouble() ;
+
+        double RoomLength;
+        System.out.println("Enter Length of room: ");
+        RoomLength = user_input.nextDouble() ;
+
+        int Dulux_PricePerLitre = 4;
+        int CraigAndRose_PricePerLitre = 16;
+        int FarrowBall_PricePerLitre = 20;
+        double AmountOfPaint_In_A_Tin_ml = 2500;
+        double RoomSurfaceArea = (2 * RoomLength * RoomHeight + RoomLength * RoomWeidth + 2 * RoomHeight * RoomWeidth);
+        int AmountOfPaintRequired_PerMetreSquared_In_ml = 100;
+        double AmountOfPaintRequiredToPaintRoom_in_ml = AmountOfPaintRequired_PerMetreSquared_In_ml * RoomSurfaceArea;
 
 
-        System.out.println(Amount_of_paint_required_to_paint_room_in_ml+"ml");
+        System.out.println("Amount of paint needed "+AmountOfPaintRequiredToPaintRoom_in_ml + " ml");
+    // Type of paint to be used
+        String Type_of_paint = "Dulux";
+        switch (Type_of_paint) {
+            case "Dulux":
+                System.out.println("Dulux is £"+ Dulux_PricePerLitre + " per Litre");
+                break;
+            case "CraigAndRose":
+                System.out.println("CraigAndRose is £" + CraigAndRose_PricePerLitre+ " per Litre");
+                break;
+            case "Farrow_Ball":
+                System.out.println("Farrow_Ball is £" + FarrowBall_PricePerLitre+" per Litre");
+        }
+    // Taking in account for windows,switches and doors area in m^2
+
+    double[] obstructions_area =  {0.0036,1.2,2}; // Array containing {area of switch,area of window, area of door}
+     for (int i = 0;i<obstructions_area.length;i++)   {
+         RoomSurfaceArea = RoomSurfaceArea - obstructions_area[i];
+
+     }
+        System.out.println("Considering obstruction area required to be painted is "+RoomSurfaceArea+" m^2");
+
+
+     // Number of Paint tins required
+    for (int Number_of_Tins=0;AmountOfPaintRequiredToPaintRoom_in_ml>0;Number_of_Tins++)
+    {
+        AmountOfPaintRequiredToPaintRoom_in_ml = AmountOfPaintRequiredToPaintRoom_in_ml- AmountOfPaint_In_A_Tin_ml;
+        if (AmountOfPaintRequiredToPaintRoom_in_ml<0)
+        System.out.println("We need " + (Number_of_Tins+1) +" Tins of 2.5L paint");
+    }
+
+
+
+
+
+
     }
 }
